4ae0556bbd3cc461f6e022d2520f1bc8dcbd51b7
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-04-15 16:33:57 +0600

    update template

--- src/main/kotlin/dev/yeff/orbital/example/scenes/MainScene.kt
+++ src/main/kotlin/dev/yeff/orbital/example/scenes/MainScene.kt
@@ -1,8 +1,13 @@
 package dev.yeff.orbital.example.scenes
 
 import dev.yeff.orbital.Game
+import dev.yeff.orbital.ecs.GameObject
+import dev.yeff.orbital.ecs.builders.GameObjectBuilder
+import dev.yeff.orbital.example.components.ColorChanger
+import dev.yeff.orbital.example.components.PlayerMovement
 import dev.yeff.orbital.graphics.Colors
 import dev.yeff.orbital.graphics.Renderer
+import dev.yeff.orbital.graphics.Shapes
 import dev.yeff.orbital.io.Input
 import dev.yeff.orbital.io.Keyboard
 import dev.yeff.orbital.io.Keys
@@ -11,38 +16,39 @@ import dev.yeff.orbital.math.Vector2f
 import dev.yeff.orbital.scenes.Scene
 import dev.yeff.orbital.util.Log
 
-class MainScene : Scene {
+class MainScene : Scene() {
+    private lateinit var player: GameObject
     private lateinit var pos: Vector2f
     private var redOrBlue: Boolean = true
     
-    private val SPEED = 13
 
     override fun init(game: Game?) {
         Log.info(javaClass, "Initialize main scene")
         pos = game!!.screenCenter
+
+        player = GameObjectBuilder()
+            .withTransform(game.screenCenter, Vector2f(20.0f, 20.0f))
+            .withShape(Shapes.CIRCLE, Colors.RED)
+            .withComponents(PlayerMovement(), ColorChanger())
+            .build()
+
+        addGameObject(game, player)
     }
 
     override fun update(game: Game?, fps: Float) {
-        val kb: Keyboard = Input.getKeyboard()
-        val mouse: Mouse = Input.getMouse()
-        
-        if (kb.isKeyDown(Keys.W)) pos.y -= SPEED
-        if (kb.isKeyDown(Keys.A)) pos.x -= SPEED
-        if (kb.isKeyDown(Keys.S)) pos.y += SPEED
-        if (kb.isKeyDown(Keys.D)) pos.x += SPEED
-        if (mouse.isMouseDown(Keys.MOUSE_MIDDLE)) redOrBlue = !redOrBlue
-
-        Renderer.drawCircle(getColor(), pos, 20.0f)
+//        val kb: Keyboard = Input.getKeyboard()
+//        val mouse: Mouse = Input.getMouse()
+//
+//        if (kb.isKeyDown(Keys.W)) pos.y -= SPEED
+//        if (kb.isKeyDown(Keys.A)) pos.x -= SPEED
+//        if (kb.isKeyDown(Keys.S)) pos.y += SPEED
+//        if (kb.isKeyDown(Keys.D)) pos.x += SPEED
+//        if (mouse.isMouseDown(Keys.MOUSE_MIDDLE)) redOrBlue = !redOrBlue
+//
+//        Renderer.drawCircle(getColor(), pos, 20.0f)
     }
 
     override fun dispose(game: Game?) {
         Log.info(javaClass, "Dispose main scene")
-    }
-
-    private fun getColor(): Colors {
-        return when {
-            redOrBlue -> Colors.RED
-            else -> Colors.BLUE
-        };
     }
 }
