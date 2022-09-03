99e7157cf074fd03579a309fe1951de24c141793
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-09-03 19:47:16 +0800

    use Orbital extension library in kotlin build

--- src/main/kotlin/dev/yeff/orbital/example/scenes/MainScene.kt
+++ src/main/kotlin/dev/yeff/orbital/example/scenes/MainScene.kt
@@ -4,12 +4,12 @@ import dev.yeff.orbital.Game
 import dev.yeff.orbital.ecs.GameObject
 import dev.yeff.orbital.ecs.builders.GameObjectBuilder
 import dev.yeff.orbital.example.components.ColorChanger
-import dev.yeff.orbital.example.components.PlayerMovement
 import dev.yeff.orbital.graphics.Color
 import dev.yeff.orbital.graphics.Shapes
 import dev.yeff.orbital.math.Vector2f
 import dev.yeff.orbital.scenes.Scene
 import dev.yeff.orbital.util.Log
+import dev.yeff.orbital.extensions.components.MovementComponent
 
 class MainScene : Scene() {
     private lateinit var player: GameObject
@@ -24,7 +24,7 @@ class MainScene : Scene() {
         player = GameObjectBuilder(this, "Player")
             .withTransform(game.screenCenter, Vector2f(20.0f, 20.0f))
             .withShape(Shapes.CIRCLE, Color.RED)
-            .withComponents(PlayerMovement(), ColorChanger())
+            .withComponents(MovementComponent(13.0f), ColorChanger())
             .build()
 
         addGameObject(game, player)
